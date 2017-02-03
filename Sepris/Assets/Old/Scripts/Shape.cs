53acfa363f9657df02c03bfbf08c173be3dea804
David Vink <david.vink@hotmail.com>
2017-02-03 14:31:21 +0100

    Made new project, Integrated a little old part.
    Started a new project due to system revamp going from raycast based to
    2d array based

---
+++ Sepris/Assets/Old/Scripts/Shape.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour {
+    public float moveTime;
+    public float fallTime;
+    public float boostTime;
+
+    bool move = true;
+    bool bottom = false;
+    public bool square = false;
+
+    public List<Transform> blocks = new List<Transform>();
+
+    Coroutine fall;
+
+	void Start () {
+        fall = StartCoroutine(Fall(fallTime));
+        int a = 0;
+        foreach (Transform childs in transform) {
+            blocks.Add(transform.GetChild(a));
+            a++;
+        }
+	}
+    void Update() {
+        Debug.DrawRay(blocks[0].position, -transform.parent.transform.up, Color.red);
+    }
+	public void Down() {
+        Vector3 downVector = new Vector3(0, 0.5f, 0);
+        transform.position -= downVector;
+    }
+    public void Boost (bool b) {
+        if (b) {
+            StopCoroutine(fall);
+            fall = StartCoroutine(Fall(boostTime));
+        }
+        else {
+            StopCoroutine(fall);
+            fall = StartCoroutine(Fall(fallTime));
+        }
+
+    }   
+    public IEnumerator Fall(float time) {
+        yield return new WaitForSeconds(time);
+        if (CheckFall())
+            yield break;
+        else {
+            Down();
+            fall = StartCoroutine(Fall(time));
+        }
+
+    }
+    public bool CheckFall() {
+        bool hit = false;
+        if (!bottom) {
+
+            RaycastHit rayHit;
+            for (int a = 0; a < blocks.Count; a++) {
+                if (Physics.Raycast(blocks[a].position, -transform.parent.transform.up, out rayHit, 0.6f)) {
+                    if (rayHit.transform.tag == "bottom") {
+                        hit = true;
+                        TetrisManager.manager.shape = null;
+                        TetrisManager.manager.SpawnNew();
+                        for (int b = 0; b < blocks.Count; b++) {
+                            blocks[b].tag = "bottom";
+
+                        }
+                        bottom = true;
+                        TetrisManager.manager.CheckField();
+                        StopAllCoroutines();
+                        return hit;
+                        
+
+                    }
+                }
+            }
+            
+        }
+        return hit;
+    }
+    public void Move(bool left) {
+        if (move) {
+            move = false;
+            StartCoroutine(MoveDelay(moveTime));
+            if (!CheckMove(left)) {
+                if (left) {
+                    Vector3 downVector = new Vector3(0.5f, 0, 0);
+                    transform.parent.transform.position -= downVector;
+                }
+                else {
+                    Vector3 downVector = new Vector3(-0.5f, 0, 0);
+                    transform.parent.transform.position -= downVector;
+                }
+            }
+        }
+    }
+    IEnumerator MoveDelay(float time) {
+        yield return new WaitForSeconds(time);
+        move = true;
+    }
+    public void Rotate(bool turn) {
+        if (!square) {
+            if (turn) { 
+                transform.Rotate(new Vector3(0, 0, 90));
+                RaycastHit hit;
+                Vector3 kick = new Vector3(0.5f, 0, 0);
+                Vector3 kick2 = new Vector3(0, 0.5f, 0);
+                for (int a = 0; a < blocks.Count; a++) {
+                    if(Physics.Raycast(blocks[a].position,transform.forward,out hit, 0.2f)) {
+                        if(hit.transform.tag == "bottom") {
+                            Rotate(false);
+                            return;
+                        }
+                    }
+                    else if(blocks[a].position.x < -2.25) {
+                        transform.parent.transform.position += kick;
+                        return;
+                    }
+                    else if (blocks[a].position.x > 2.25) {
+                        transform.parent.transform.position -= kick;
+                        return;
+                    }
+                    else if(blocks[a].position.y < -5) {
+                        transform.parent.transform.position += kick2;
+                        return;
+                    }
+
+                }
+            }
+            else {
+                transform.Rotate(new Vector3(0, 0, -90));
+            }
+        }
+
+        
+    }
+    public bool CheckMove(bool side) {
+        bool hit = false;
+        RaycastHit rayHit;
+        Vector3 sideVector;
+        if (side) {
+            sideVector = -transform.parent.transform.right;
+        }
+        else {
+            sideVector = transform.parent.transform.right;
+        }
+        for (int a = 0; a < blocks.Count; a++) {
+            if (Physics.Raycast(blocks[a].position, sideVector, out rayHit, 0.6f)) {
+                if (rayHit.transform.tag == "Side" || rayHit.transform.tag == "bottom") {
+                    hit = true;
+
+                }
+            }
+        }
+        return hit;
+    
+    }
+}
---
+++ Sepris/Assets/Old/Scripts/TetrisManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisManager : MonoBehaviour {
+    public static TetrisManager manager;
+    public Shape shape;
+    public Vector3[] spawnPos;
+    public GameObject[] shapes;
+    public Transform[] checkers;
+
+    public int next;
+
+    void Start() {
+        SpawnNew();
+        next = Random.Range(0, shapes.Length);
+        manager = this;
+
+    }
+    void Update() {
+        Controls();
+    }
+    public void SpawnNew() {
+        Shape newShape = (Shape)Instantiate(shapes[next], spawnPos[Random.Range(0, spawnPos.Length)], Quaternion.identity).transform.GetChild(0).GetComponent<Shape>();
+        next = Random.Range(0, shapes.Length);
+        shape = newShape;
+        //UIManager.UI.NextUpdate(next);
+    }
+    void Controls() {
+        if (Input.GetButtonDown("Down")){
+            shape.Boost(true);
+
+        }
+        if (Input.GetButtonUp("Down")) {
+            shape.Boost(false);
+        }
+        if (Input.GetButtonDown("Up")) {
+            shape.Rotate(true);
+        }
+        if (Input.GetButton("Left")) {
+            shape.Move(true);
+        }
+        else if (Input.GetButton("Right")) {
+            shape.Move(false);
+        }
+    }
+    public void CheckField() {
+
+    }
+}
